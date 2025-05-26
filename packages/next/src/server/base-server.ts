0ef042738470bd5337413fe5de5d50d9fb13b54e
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-05-26 14:32:48 -0600

    fix: don't send the postpone header on dynamic RSC requests

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -3638,7 +3638,7 @@ export default abstract class Server<
       }
 
       // Mark that the request did postpone.
-      if (didPostpone) {
+      if (didPostpone && !isDynamicRSCRequest) {
         res.setHeader(NEXT_DID_POSTPONE_HEADER, '1')
       }
 
